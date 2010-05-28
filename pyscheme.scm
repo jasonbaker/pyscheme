f504456005dedbcfd98dc455746912e405946d92
Jason Baker <amnorvend@gmail.com>
2010-05-28 12:57:42 -0500

    Added Py_Finalize

--- pyscheme.scm
+++ pyscheme.scm
@@ -7,26 +7,21 @@
   (define (init [lib "libpython"])
     (set! libpython (ffi-lib lib)))
 
-  (define (id->string id)
-    (symbol->string (quote id)))
-    
-
   (define-syntax (define-cpyfunc stx)
     (syntax-case stx ()
                  [(define-cpyfunc id type)
                   #'(define-values (id) 
                                    (values (get-cpyfunc (symbol->string (quote id))
-                                                        type)))]))
+                                                        type)))
+                           ]))
 
+    (define (get-cpyfunc name type)
+      (lambda args
+        (if libpython
+          (apply (get-ffi-obj name libpython type) args)
+          (error "Call init before using any Python C functions"))))
 
-
-  (define (get-cpyfunc name type)
-    (lambda args
-      (if libpython
-        (apply (get-ffi-obj name libpython type) args)
-        (error "Call init before using any Python C functions"))))
-  
-  (define-cpyfunc Py_Initialize (_fun -> _void))
-  (provide init Py_Initialize define-cpyfunc)
-
-  ) 
+    (define-cpyfunc Py_Initialize (_fun -> _void))
+    (define-cpyfunc Py_Finalize (_fun -> _void))
+    (provide (all-defined-out))
+    ) 
